cc46c7e6c0876901c150d2aa7ead650f300157ce
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-04-18 12:22:43 +0200

    session migration changed -> removed signature

--- backend/migrations/2023-04-18-093641_session_v1/up.sql
+++ backend/migrations/2023-04-18-093641_session_v1/up.sql
@@ -4,7 +4,6 @@ CREATE TABLE sessions (
 	session_id uuid NOT NULL PRIMARY KEY,
 	user_id uuid NOT NULL,
 	expires_at timestamp NOT NULL,
-	signature text,
 	CONSTRAINT fk_user_id 
 		FOREIGN KEY (user_id) 
 		REFERENCES users(id)
