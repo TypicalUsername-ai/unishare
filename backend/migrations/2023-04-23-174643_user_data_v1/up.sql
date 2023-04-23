8f3ed2cba9e75b4e4ab4e645a93a511c1f0d2390
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-04-23 20:39:57 +0200

    migrations sql code

---
+++ backend/migrations/2023-04-23-174643_user_data_v1/up.sql
@@ -0,0 +1,11 @@
+-- Your SQL goes here
+
+CREATE TABLE users_data (
+	user_id uuid NOT NULL PRIMARY KEY,
+	pub_files integer NOT NULL DEFAULT 0,
+	priv_files integer NOT NULL DEFAULT 0,
+	tokens integer NOT NULL DEFAULT 0,
+	CONSTRAINT fk_user_id
+		FOREIGN KEY (user_id)
+		REFERENCES users(id)
+);
---
+++ backend/migrations/2023-04-23-174822_reviews_v1/up.sql
@@ -0,0 +1,15 @@
+-- Your SQL goes here
+
+CREATE TABLE user_reviews (
+	reviewed_id uuid NOT NULL,
+	reviewer_id uuid NOT NULL,
+	review integer NOT NULL,
+	comment text DEFAULT NULL,
+	CONSTRAINT fk_reviewed
+		FOREIGN KEY (reviewed_id)
+		REFERENCES users(id),
+	CONSTRAINT fk_reviewer
+		FOREIGN KEY (reviewer_id)
+		REFERENCES users(id),
+	PRIMARY KEY (reviewed_id, reviewer_id)
+);
