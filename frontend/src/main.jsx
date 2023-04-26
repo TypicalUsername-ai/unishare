092dbf50af2357961e1519f14198a18ff954ff2e
Ernest Strychalski <63451081+ejdej1@users.noreply.github.com>
2023-04-26 19:59:27 +0200

    redux isLogged

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,15 +7,23 @@ import {
 import './index.css'
 import routes from './routes'
 import NavMenu from './components/NavMenu'
+import {createStore} from 'redux'
+import allReducers from './reducers'
+import {Provider} from 'react-redux'
+
+const store = createStore(allReducers,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const router = createBrowserRouter(routes, {basename: '/app/'});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-	<div className="TopMenu">
-		<div> UniShare </div>
-		<NavMenu/>
-	</div>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+<Provider store={store}>
+	<React.StrictMode>
+		<div className="TopMenu">
+			<div> UniShare </div>
+			<NavMenu/>
+		</div>
+		<RouterProvider router={router}/>
+	</React.StrictMode>,
+</Provider>
 )
