e276db97a56911cf3dcbedb2be4e88511cab82c6
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-06-18 20:00:17 +0200

    brought user files service into scope

--- backend/src/services/user_services.rs
+++ backend/src/services/user_services.rs
@@ -16,6 +16,7 @@ pub fn config(cfg: &mut web::ServiceConfig) {
              .service(get_reviews)
              .service(add_review)
              .service(search)
+             .service(get_files)
             );
 }
 
