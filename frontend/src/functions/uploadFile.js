013695b16ac66b36045382afca9558e20fd64fb7
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-06-22 00:04:18 +0200

    work on file uploads

---
+++ frontend/src/functions/uploadFile.js
@@ -0,0 +1,17 @@
+async function uploadFile(filedata, token) {
+    console.log(filedata);
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization' : `Bearer ${token}`,
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify(filedata)
+    };
+
+    let response = await fetch(`http://localhost/api/files/create`, options)
+    if (!response.ok) {throw new Error("UploadFile : " + response.status)}
+    return await response.json()
+}
+
+export default uploadFile
