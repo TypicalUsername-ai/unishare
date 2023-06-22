70eb3ef794d8f2aa7e8b8b43280ca90df8c10cce
Taras Zhyhulin <zhyhulin@student.agh.edu.pl>
2023-06-22 11:14:49 +0200

    added documentation

--- backend/src/entities/transaction.rs
+++ backend/src/entities/transaction.rs
@@ -7,16 +7,23 @@ use diesel::{Insertable, Queryable};
 #[diesel(table_name = transactions)]
 pub struct Transaction {
     pub id: Uuid,
-    pub transaction_type: TransactionType
+    pub transaction_type: TransactionType,
+    pub creator_id: Uuid,
+    pub buyer_id: Uuid,
+    pub file_id: Uuid,
+    pub transaction_time: SystemTime,
+    pub price: i32
 }
 
+// Enumeration of transaction types
+// more types to be added later
 pub enum TransactionType {
     purchase
 }
 
 impl Transaction {
     
-    //// Simple function checking whether a user with provided 'Uuid' owns the file identified by the provided `Uuid`
+    // Simple function checking whether a user with provided 'Uuid' owns the file identified by the provided `Uuid`
     pub async fn user_owns_file(file_id: Uuid, user_id: Uuid, db_conn: &mut PgConnection) -> Result<bool, UnishareError> {
         let user_is_owner = transactions::table
             .filter(transactions::buyer_id.eq(user_id.clone())
