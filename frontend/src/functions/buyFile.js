460c564d45cbba843cfa34c88ec3ec652ea2cf5d
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-06-27 17:48:27 +0200

    fixed buyFile function

--- frontend/src/functions/buyFile.js
+++ frontend/src/functions/buyFile.js
@@ -3,20 +3,12 @@ async function buyFile(userId, fileId, authToken) {
     const response = await fetch(`http://localhost/api/files/${fileId}/buy`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
             'Authorization': `Bearer ${authToken}`
         },
-        body: JSON.stringify({
-            buyerId: userId,
-            fileId: fileId
-        })
     });
 
-    if (response.ok) {
-        alert("You have bought the file and should now have access to it!");
-    } else {
-        console.error('Error buying the file, reason: ' + response);
-    }
+    if (!response.ok) {throw new Error("purchaseFile : "+response.status)}
+    return await response.json()
 }
 
 export default buyFile;
