e9126ffec712f6449a3a5030578eda413521f2fd
Mateusz Domalewski <mati.domalewski@gmail.com>
2023-06-29 10:16:57 +0200

    semi working

--- frontend/src/components/admin/FileList.jsx
+++ frontend/src/components/admin/FileList.jsx
@@ -3,26 +3,15 @@ import ReportedFile from "./ReportedFile";
 
 const FileList = ({ data = [] }) => {
 
+    const [filtered, setFiltered] = useState(data);
+
     return (
         <div style={{padding:"20px", background:"#ADD8E6"}}>
             <h3>Reported Files</h3>
             <div>
-                <p> File Id </p>
-            <select name="reporter" selected="none">
-                <option value="none"> None </option>
-                {data.map((entry) => {
-                    <option value={entry.reporter_id}>{entry.reporter_id}</option>
-                })}
-            </select>
-                <p> Reason </p>
-            <select name="reason">
-                <option> None </option>
-                {data.map((entry) => {
-                    <option value={entry.reason}>{entry.reason}</option>
-                })}
-            </select>
+                <input type="text" onChange={(e) => setFiltered(data.filter((entry) => entry.reason.contains(e.target.value)))}/>
             </div>
-            {data.map(
+            {filtered.map(
                 (entry) => <ReportedFile 
                     fileid={entry.object_id}
                     reporter_id={entry.reporter_id}
