cba22c9dd4dbb2c8105bed0b59213b3de4b5737e
Ernest Strychalski <63451081+ejdej1@users.noreply.github.com>
2023-06-29 11:17:00 +0300

    confirmy

--- frontend/src/functions/acceptReport.js
+++ frontend/src/functions/acceptReport.js
@@ -9,6 +9,10 @@ export default async function acceptReport (id, token){
         console.log("ID accept Report" + id)
         console.log("Token accept Report" + token)
 
-    let response = await fetch(`http://localhost/api/reports/${id}/accept`, options)
-    if (!response.ok) {throw new Error("Accept Report : " + response.status)}
+
+    let text = "Are you sure you want to delete this account?";
+    if (confirm(text) == true) {
+        let response = await fetch(`http://localhost/api/reports/${id}/accept`, options)
+        if (!response.ok) {throw new Error("Accept Report : " + response.status)}
+    }
 }
